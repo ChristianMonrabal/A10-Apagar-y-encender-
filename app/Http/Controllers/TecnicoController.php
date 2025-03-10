7c26f36540f18b872b55bc4cbdc99cd717e5ab51
danielbecee <dabe6612@gmail.com>
2025-03-10 16:03:12 +0100

    merge

--- app/Http/Controllers/TecnicoController.php
+++ app/Http/Controllers/TecnicoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\Incidence;
+use App\Models\Incidencia;
 
 class TecnicoController 
 {
