fb86f021753dc17c4c81751a2e4b79150c37ff0e
pol-mrk <polmarc.monro@gmail.com>
2025-03-10 20:37:38 +0100

    cambios_gestor

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,7 @@ Route::get('/client', function () {
     }
 })->name('client');
 
-Route::get('/manager', function () {
-    if (Auth::check() && Auth::user()->roles_id === 3) {
-        return view('manager.manager');
-    } else {
-        return redirect('/');
-    }
-})->name('manager');
+Route::get('/manager', [GestorController::class, 'incidencias'])->name('manager');
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
